78edacf9f5a547df8f4b5c178d439b0575b8d93c
Naushaba Fatima <naushaba.fatima@ryerson.ca>
2015-10-26 16:55:03 -0400

    hello

--- lab5/src/lab5.cpp
+++ lab5/src/lab5.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello Wrld!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello Wr!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
